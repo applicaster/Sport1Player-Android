94d1892b0c037fa915befcc4160d19e3e5a39955
amykh <amykh@scand.com>
2019-11-13 16:24:16 +0300

    fixed bug with player sound playing on PIN validation screen

--- src/main/java/com/applicaster/sport1player/Sport1PlayerActivity.java
+++ src/main/java/com/applicaster/sport1player/Sport1PlayerActivity.java
@@ -1,12 +1,15 @@
 package com.applicaster.sport1player;
 
+import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.support.annotation.Nullable;
 
+import com.applicaster.app.SpecificActivityLifecycleCallbacks;
 import com.applicaster.jwplayerplugin.JWPlayerActivity;
+import com.applicaster.jwplayerplugin.JWPlayerContainer;
 import com.applicaster.plugin_manager.PluginManager;
 import com.applicaster.plugin_manager.playersmanager.Playable;
 import com.applicaster.pluginpresenter.PluginPresenter;
@@ -30,6 +33,8 @@ public class Sport1PlayerActivity extends JWPlayerActivity {
     private CountDownTimer refreshTimer = null;
     private boolean videoValidated;
 
+    private SpecificActivityLifecycleCallbacks activityLifecycleCallbacks = null;
+
     public static void startPlayerActivity(Context context, Bundle bundle, Map<String, String> params) {
         Intent intent = new Intent(context, Sport1PlayerActivity.class);
         intent.putExtras(bundle);
@@ -47,24 +52,41 @@ public class Sport1PlayerActivity extends JWPlayerActivity {
         wasPaused = false;
         //  it is validated in adapter before starting this activity if not free
         videoValidated = !playable.isFree();
+
+        activityLifecycleCallbacks = new SpecificActivityLifecycleCallbacks(this, new NavigationLifecycleCallbacks() {
+            @Override
+            public void onActivityResumed(Activity activity) {
+                if (wasPaused && videoValidated) {
+                    //  present PIN validation again after restore from background
+                    wasPaused = false;
+                    presentValidationPlugin();
+                }
+                setupNextRefresh();
+                setupNextValidation();
+            }
+
+            @Override
+            public void onActivityPaused(Activity activity) {
+                wasPaused = true;
+                clearNextValidation();
+            }
+
+            @Override
+            public void onActivityStopped(Activity activity) {
+                //  pause player while PIN screen is displayed
+                JWPlayerContainer container = findViewById(R.id.playerView);
+                if (container != null)
+                    container.getJWPlayerView().pause();
+            }
+        });
+        getApplication().registerActivityLifecycleCallbacks(activityLifecycleCallbacks);
     }
 
     @Override
-    protected void onStart() {
-        if (wasPaused && videoValidated) {
-            wasPaused = false;
-            presentValidationPlugin();
-        }
-        setupNextRefresh();
-        setupNextValidation();
-        super.onStart();
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        wasPaused = true;
-        clearNextValidation();
+    protected void onDestroy() {
+        getApplication().unregisterActivityLifecycleCallbacks(activityLifecycleCallbacks);
+        activityLifecycleCallbacks = null;
+        super.onDestroy();
     }
 
     @Override
